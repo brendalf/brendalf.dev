c4c74352640f0ad8f711eeefc13b77ba32edb545
Breno Silva <brenophp@gmail.com>
2023-12-09 01:19:55 +0100

    fix: update package-lock after deleting next-themes

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,15 +1,8 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
-import { ThemeProvider } from "next-themes";
 import { ReactNode } from "react";
 
 export function Providers({ children }: { children: ReactNode }) {
-  return (
-    <NextUIProvider>
-      <ThemeProvider attribute="class" defaultTheme="dark">
-        {children}
-      </ThemeProvider>
-    </NextUIProvider>
-  );
+  return <NextUIProvider>{children}</NextUIProvider>;
 }
