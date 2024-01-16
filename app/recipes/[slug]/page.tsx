3c9b28eebffd98fd5ac9cbcad1593a2962f82ea8
Breno Silva <brenophp@gmail.com>
2024-01-16 01:12:35 +0100

    feat: finish recipe view page with chocolate cookie recipe

--- app/recipes/[slug]/page.tsx
+++ app/recipes/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import recipes from "@/app/recipes/data";
-import { Image, Spacer } from "@nextui-org/react";
+import { faBlender, faPlateWheat } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Divider, Image, Spacer } from "@nextui-org/react";
 
 export default async function ViewRecipe({
   params,
@@ -13,7 +15,7 @@ export default async function ViewRecipe({
   }
 
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col justify-center">
       <Image
         className="object-cover h-[350px] w-screen"
         alt={recipe.title}
@@ -24,8 +26,27 @@ export default async function ViewRecipe({
       <h2 className="px-4 mb-2 text-xl text-center font-light">
         {recipe.title}
       </h2>
-      <div>
-        <span>{recipe.content}</span>
+      <div className="w-2/5 container mx-auto">
+        <div className="text-lg">
+          <FontAwesomeIcon icon={faPlateWheat} />
+          <span className="ml-4">Ingredients</span>
+        </div>
+        <Divider className="my-2" />
+        <div className="text-gray-300 font-light">
+          {recipe.ingredients.map((value, idx) => (
+            <div key={idx}>{value}</div>
+          ))}
+        </div>
+        <div className="text-lg mt-8">
+          <FontAwesomeIcon icon={faBlender} />
+          <span className="ml-4">How to make it</span>
+        </div>
+        <Divider className="my-2" />
+        {recipe.process.map((value, idx) => (
+          <div key={idx} className="text-gray-300 font-light mb-4">
+            {value}
+          </div>
+        ))}
       </div>
     </div>
   );
